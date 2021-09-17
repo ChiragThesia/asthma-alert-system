72cbe2dd87065b4fc5f716951368e8ef9e2ecff9
Chirag Thesia <harithesia@gmail.com>
2021-09-17 12:15:37 +0400

    Got the primary function working to allow the user to provide locaiton and alert preference

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataGather from './components/DataCall';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>Asthma Alert System</header>
+      <DataGather />
+    </div>
+  );
+}
+
+export default App;
